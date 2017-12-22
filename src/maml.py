921ce8d813a0dbce899828b230c34a46f9639bed
CEfanmin <fanminUESTC2015@163.com>
2017-12-22 11:12:16 +0800

    fix some based on maml

---
+++ src/maml.py
@@ -0,0 +1,143 @@
+""" Code for the MAML algorithm and network definitions. """
+from __future__ import print_function
+import numpy as np
+import sys
+import tensorflow as tf
+try:
+    import special_grads
+except KeyError as e:
+    print('WARN: Cannot define MaxPoolGrad, likely already defined for this version of tensorflow: %s' % e,
+          file=sys.stderr)
+
+from tensorflow.python.platform import flags
+from utils import mse, xent, conv_block, normalize
+
+FLAGS = flags.FLAGS
+
+class MAML:
+    def __init__(self, dim_input=1, dim_output=1, test_num_updates=5):
+        """ must call construct_model() after initializing MAML! """
+        self.dim_input = dim_input
+        self.dim_output = dim_output
+        self.update_lr = FLAGS.update_lr  # inner learning rate: alpha
+        self.meta_lr = tf.placeholder_with_default(FLAGS.meta_lr, ()) # meta learning rate: beta
+        self.test_num_updates = test_num_updates
+        if FLAGS.datasource == 'sinusoid':
+            self.dim_hidden = [40, 40]
+            self.loss_func = mse
+            self.forward = self.forward_fc
+            self.construct_weights = self.construct_fc_weights
+        else:
+            raise ValueError('Unrecognized data source.')
+
+    def construct_model(self, input_tensors=None, prefix='metatrain_'):
+        # a: training data for inner gradient, b: test data for meta gradient
+        if input_tensors is None:
+            self.inputa = tf.placeholder(tf.float32)
+            self.inputb = tf.placeholder(tf.float32)
+            self.labela = tf.placeholder(tf.float32)
+            self.labelb = tf.placeholder(tf.float32)
+        else:
+            self.inputa = input_tensors['inputa']
+            self.inputb = input_tensors['inputb']
+            self.labela = input_tensors['labela']
+            self.labelb = input_tensors['labelb']
+
+        # load pre model weight
+        with tf.variable_scope('model', reuse=None) as training_scope:
+            if 'weights' in dir(self):
+                training_scope.reuse_variables()
+                weights = self.weights
+            else:
+                # Re-define the weights
+                self.weights = weights = self.construct_weights()
+
+            # outputbs[i] and lossesb[i] is the output and loss after i+1 gradient updates
+            lossesa, outputas, lossesb, outputbs = [], [], [], []
+            num_updates = max(self.test_num_updates, FLAGS.num_updates)
+            outputbs = [[]]*num_updates
+            lossesb = [[]]*num_updates
+
+            def task_metalearn(inp, reuse=True):
+                """ Perform gradient descent for one task in the meta-batch. """
+                inputa, inputb, labela, labelb = inp
+                task_outputbs, task_lossesb = [], []
+
+                task_outputa = self.forward(inputa, weights, reuse=reuse)  # only reuse initial weights on the first iter
+                task_lossa = self.loss_func(task_outputa, labela)
+
+                grads = tf.gradients(task_lossa, list(weights.values()))
+                if FLAGS.stop_grad:
+                    grads = [tf.stop_gradient(grad) for grad in grads]
+                gradients = dict(zip(weights.keys(), grads))
+                fast_weights = dict(zip(weights.keys(), [weights[key] - self.update_lr*gradients[key] for key in weights.keys()]))
+                output = self.forward(inputb, fast_weights, reuse=True)
+                task_outputbs.append(output)
+                task_lossesb.append(self.loss_func(output, labelb))
+                print("outter loop")
+                for j in range(num_updates - 1):
+                    print("task meta-learn j is: ", j)
+                    loss = self.loss_func(self.forward(inputa, fast_weights, reuse=True), labela)
+                    grads = tf.gradients(loss, list(fast_weights.values()))
+                    if FLAGS.stop_grad:
+                        grads = [tf.stop_gradient(grad) for grad in grads]
+                    gradients = dict(zip(fast_weights.keys(), grads))
+                    fast_weights = dict(zip(fast_weights.keys(), [fast_weights[key] - self.update_lr*gradients[key] for key in fast_weights.keys()]))
+                    # b used for meta learner test loss
+                    output = self.forward(inputb, fast_weights, reuse=True)
+                    task_outputbs.append(output)
+                    task_lossesb.append(self.loss_func(output, labelb))
+
+                task_output = [task_outputa, task_outputbs, task_lossa, task_lossesb]
+                return task_output
+
+            if FLAGS.norm is not 'None':
+                # to initialize the batch norm vars, might want to combine this, and not run idx 0 twice.
+                unused = task_metalearn((self.inputa[0], self.inputb[0], self.labela[0], self.labelb[0]), False)
+
+            out_dtype = [tf.float32, [tf.float32]*num_updates, tf.float32, [tf.float32]*num_updates]
+            result = tf.map_fn(task_metalearn, elems=(self.inputa, self.inputb, self.labela, self.labelb), dtype=out_dtype, parallel_iterations=FLAGS.meta_batch_size)
+
+            outputas, outputbs, lossesa, lossesb  = result
+
+        ## Performance & Optimization
+        if 'train' in prefix:
+            self.total_loss1 = total_loss1 = tf.reduce_sum(lossesa) / tf.to_float(FLAGS.meta_batch_size)
+            self.total_losses2 = total_losses2 = [tf.reduce_sum(lossesb[j]) / tf.to_float(FLAGS.meta_batch_size) for j in range(num_updates)]
+            # after the map_fn
+            self.outputas, self.outputbs = outputas, outputbs
+            self.pretrain_op = tf.train.AdamOptimizer(self.meta_lr).minimize(total_loss1)
+
+            if FLAGS.metatrain_iterations > 0:
+                optimizer = tf.train.AdamOptimizer(self.meta_lr)
+                self.gvs = gvs = optimizer.compute_gradients(self.total_losses2[FLAGS.num_updates-1])
+                self.metatrain_op = optimizer.apply_gradients(gvs)
+        else:
+            self.metaval_total_loss1 = total_loss1 = tf.reduce_sum(lossesa) / tf.to_float(FLAGS.meta_batch_size)
+            self.metaval_total_losses2 = total_losses2 = [tf.reduce_sum(lossesb[j]) / tf.to_float(FLAGS.meta_batch_size) for j in range(num_updates)]
+            
+
+        ## Summaries
+        tf.summary.scalar(prefix+'Pre-update loss', total_loss1)
+        for j in range(num_updates):
+            tf.summary.scalar(prefix+'Post-update loss, step ' + str(j+1), total_losses2[j])
+            
+    ### Network construction functions:fc networks
+    def construct_fc_weights(self):
+        weights = {}
+        weights['w1'] = tf.Variable(tf.truncated_normal([self.dim_input, self.dim_hidden[0]], stddev=0.01))
+        weights['b1'] = tf.Variable(tf.zeros([self.dim_hidden[0]]))
+        for i in range(1,len(self.dim_hidden)):
+            weights['w'+str(i+1)] = tf.Variable(tf.truncated_normal([self.dim_hidden[i-1], self.dim_hidden[i]], stddev=0.01))
+            weights['b'+str(i+1)] = tf.Variable(tf.zeros([self.dim_hidden[i]]))
+        weights['w'+str(len(self.dim_hidden)+1)] = tf.Variable(tf.truncated_normal([self.dim_hidden[-1], self.dim_output], stddev=0.01))
+        weights['b'+str(len(self.dim_hidden)+1)] = tf.Variable(tf.zeros([self.dim_output]))
+        return weights
+
+    def forward_fc(self, inp, weights, reuse=False):
+        hidden = normalize(tf.matmul(inp, weights['w1']) + weights['b1'], activation=tf.nn.relu, reuse=reuse, scope='0')
+        for i in range(1,len(self.dim_hidden)):
+            hidden = normalize(tf.matmul(hidden, weights['w'+str(i+1)]) + weights['b'+str(i+1)], activation=tf.nn.relu, reuse=reuse, scope=str(i+1))
+        return tf.matmul(hidden, weights['w'+str(len(self.dim_hidden)+1)]) + weights['b'+str(len(self.dim_hidden)+1)]
+
+
